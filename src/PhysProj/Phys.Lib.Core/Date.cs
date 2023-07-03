95a1309b90adc6a7d3e66aaf3ca008e33ec03b43
Yan Tanygin <fcbshnik@gmail.com>
2023-07-04 00:07:29 +0300

    [tests] date format tests

---
+++ src/PhysProj/Phys.Lib.Core/Date.cs
@@ -0,0 +1,77 @@
+﻿using FluentValidation;
+using System.Text.RegularExpressions;
+
+namespace Phys.Lib.Core
+{
+    public class Date
+    {
+        private static Validator validator = new Validator();
+
+        private static Result<Date> parseFail = Result.Fail<Date>("invalid date");
+        private static readonly Regex parseRegex = new Regex(@"^(?<prefix>\d+)(?<approximatePower>Y+)?(?<approximateSkew>\d+)?(?<bce>BCE)?$", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.IgnoreCase);
+
+        public string Code { get; }
+
+        public int Min { get; }
+
+        public int Max { get; }
+
+        private Date(string code, int min, int max)
+        {
+            Code = code;
+            Min = min;
+            Max = max;
+        }
+
+        public static Result<Date> Parse(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return parseFail;
+
+            value = value.ToUpperInvariant();
+
+            var match = parseRegex.Match(value);
+            if (!match.Success)
+                return parseFail;
+
+            var isBeforeCommonEra = match.Groups["bce"].Value.Length > 0;
+            var prefix = int.Parse(match.Groups["prefix"].Value);
+            var approximatePower = match.Groups["approximatePower"].Value.Length;
+            if (approximatePower > 3)
+                return parseFail;
+            var approximateSkewBaseRaw = match.Groups["approximateSkew"].Value;
+            if (approximateSkewBaseRaw.Length > 3)
+                return parseFail;
+
+            var approximateSkewBase = (approximateSkewBaseRaw.Length > 0 ? int.Parse(approximateSkewBaseRaw) : 1) * 10;
+
+            if (isBeforeCommonEra)
+                prefix *= -1;
+
+            var approximateOrder = (int)Math.Pow(10, approximatePower);
+            var approximateSkew = approximatePower > 0 ? (int)Math.Pow(approximateSkewBase, approximatePower) : 0;
+            var min = prefix * approximateOrder - approximateSkew;
+            var max = prefix * approximateOrder + approximateSkew;
+
+            if (min > 2023)
+                return parseFail;
+
+            return Result.Ok(new Date(value, min, max));
+        }
+
+        internal class Validator : AbstractValidator<string>
+        {
+            public Validator()
+            {
+                RuleFor(u => u)
+                    .Must(Check)
+                    .WithMessage("date is invalid");
+            }
+
+            private bool Check(string value)
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ src/PhysProj/Phys.Lib.Tests.Unit/DateTests.cs
@@ -0,0 +1,59 @@
+﻿namespace Phys.Lib.Tests.Unit
+{
+    public class DateTests
+    {
+        [Theory]
+        [InlineData("1YYYY")]
+        [InlineData("YYBCE")]
+        [InlineData("BCE")]
+        [InlineData("-12")]
+        [InlineData("-12BCE")]
+        [InlineData("BCE12")]
+        [InlineData("1BCE2")]
+        [InlineData("+123")]
+        [InlineData("123!")]
+        [InlineData("2024")]
+        [InlineData("Y55")]
+        [InlineData("1987Y")]
+        [InlineData("204Y")]
+        public void InvalidDateTests(string date)
+        {
+            var result = Date.Parse(date);
+            result.Ok.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("5000BCE", -5000, -5000)]
+        [InlineData("35YBCE", -360, -340)]
+        [InlineData("300BCE", -300, -300)]
+        [InlineData("20BCE", -20, -20)]
+        [InlineData("1BCE", -1, -1)]
+        [InlineData("10", 10, 10)]
+        [InlineData("30", 30, 30)]
+        [InlineData("399", 399, 399)]
+        [InlineData("1546", 1546, 1546)]
+        [InlineData("2023", 2023, 2023)]
+        [InlineData("0YYBCE", -100, 100)]
+        [InlineData("0YY", -100, 100)]
+        [InlineData("3YYBCE", -400, -200)]
+        [InlineData("33Y2BCE", -350, -310)]
+        [InlineData("32YBCE", -330, -310)]
+        [InlineData("0Y", -10, 10)]
+        [InlineData("3Y", 20, 40)]
+        [InlineData("4YY", 300, 500)]
+        [InlineData("155Y", 1540, 1560)]
+        [InlineData("150Y", 1490, 1510)]
+        [InlineData("150Y5", 1450, 1550)]
+        [InlineData("15YY", 1400, 1600)]
+        [InlineData("18Y5", 130, 230)]
+        [InlineData("198Y", 1970, 1990)]
+        [InlineData("202Y", 2010, 2030)]
+        public void ValidDateTests(string date, int min, int max)
+        {
+            var result = Date.Parse(date);
+            result.Ok.Should().BeTrue();
+            result.Value.Min.Should().Be(min);
+            result.Value.Max.Should().Be(max);
+        }
+    }
+}
