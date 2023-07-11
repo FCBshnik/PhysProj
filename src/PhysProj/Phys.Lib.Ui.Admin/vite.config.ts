14153f71333f8968dff2fd62919918a194878a6d
Yan Tanygin <fcbshnik@gmail.com>
2023-07-11 17:17:21 +0300

    [ui] fixed admin url

--- src/PhysProj/Phys.Lib.Ui.Admin/vite.config.ts
+++ src/PhysProj/Phys.Lib.Ui.Admin/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 		  "/api": {
-			target: "https://localhost:7188",
+			target: "http://localhost:7188",
 			changeOrigin: true,
 			secure: false,
 		  },
