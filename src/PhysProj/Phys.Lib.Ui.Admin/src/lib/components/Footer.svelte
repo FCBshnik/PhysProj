872cc9d4a7d432f7c63fa50bc5e7c08f38f650cb
Yan Tanygin <fcbshnik@gmail.com>
2023-07-25 22:30:49 +0300

    Update Footer.svelte

--- src/PhysProj/Phys.Lib.Ui.Admin/src/lib/components/Footer.svelte
+++ src/PhysProj/Phys.Lib.Ui.Admin/src/lib/components/Footer.svelte
@@ -16,6 +16,6 @@
     setInterval(check, 10000);
 </script>
 
-<div class="absolute bottom-0 right-0 max-w-md w-full p-4 text-right">
-    api ver. {apiVersion}, time {apiTime?.toISOString()}
+<div class="absolute bottom-0 right-0 w-full p-4 text-right">
+    web. ver 2023.07.25 api ver. {apiVersion}, time {apiTime?.toISOString()}
 </div>
