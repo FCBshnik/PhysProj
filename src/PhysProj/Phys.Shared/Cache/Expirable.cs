598d222f468728a30398c5996db3a2a3cf2f5d45
Yan Tanygin <2025234+FCBshnik@users.noreply.github.com>
2023-10-22 18:07:35 +0300

    added expirable to cache single values in memory

---
+++ src/PhysProj/Phys.Shared/Cache/Expirable.cs
@@ -0,0 +1,36 @@
+﻿namespace Phys.Shared.Cache
+{
+    public class Expirable<T> where T : class
+    {
+        private readonly TimeSpan expiration;
+        private readonly Func<T> factory;
+
+        private T? value;
+        private DateTime? createdAt;
+
+        public Expirable(Func<T> factory, TimeSpan expiration)
+        {
+            this.factory = factory;
+            this.expiration = expiration;
+        }
+
+        public T Value
+        {
+            get
+            {
+                if (value == null || DateTime.UtcNow - createdAt > expiration)
+                {
+                    value = factory();
+                    createdAt = DateTime.UtcNow;
+                }
+
+                return value;
+            }
+        }
+
+        public void Reset()
+        {
+            value = default;
+        }
+    }
+}
