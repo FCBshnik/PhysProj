dec3f42b53b67f92edd71c93797f7f0b21c6b07f
Yan Tanygin <2025234+FCBshnik@users.noreply.github.com>
2023-11-21 22:45:35 +0300

    added logo

--- src/PhysProj/Phys.Lib.Site.Web/src/routes/+page.svelte
+++ src/PhysProj/Phys.Lib.Site.Web/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import * as api from '$lib/services/ApiService';
+  import logo from '$lib/assets/logo.png';
 	import { onMount } from 'svelte';
 	import WorkCard from '$lib/components/WorkCard.svelte';
 	import SearchInput from '$lib/components/SearchInput.svelte';
@@ -24,7 +25,15 @@
 <article class="dark:bg-gray-800 dark:text-gray-400 bg-gray-300 text-black">
 	<div class="p-4 flex flex-col h-full min-h-screen">
 		<div class="p-2 flex flex-row items-center justify-between">
-			<div><a href="/">PhysLib</a></div>
+			<div>
+        <a class="flex flex-row" href="/">
+          <img class="inline w-12 h-12 dark:mix-blend-luminosity brightness-150" alt="PhysLib" src={logo} />
+          <span class="pl-2 flex flex-col justify-center">
+            <span class="text-center">Phys</span>
+            <span class="text-center">Lib</span>
+          </span>
+        </a>
+      </div>
 			<div class="w-2/3 flex flex-row">
 				<div class="w-full">
 					<SearchInput bind:searchText on:onSearch={refresh} />
