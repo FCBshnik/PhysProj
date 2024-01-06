a492fd6d2084dae6f9b0c59e95be2671fb37747b
Yan Tanygin <2025234+FCBshnik@users.noreply.github.com>
2024-01-06 21:22:09 +0300

    Update EvoleMigrations.cs

--- src/PhysProj/Phys.Lib.Postgre/EvoleMigrations.cs
+++ src/PhysProj/Phys.Lib.Postgre/EvoleMigrations.cs
@@ -8,11 +8,16 @@ namespace Phys.Lib.Postgres
     {
         public static void Migrate(DbConnection cnx, ILogger logger)
         {
+            var migrationsDirectory = new DirectoryInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "db/postgres/migrations"));
+
+            logger.LogInformation($"migrations files at '{migrationsDirectory.FullName}': {migrationsDirectory.EnumerateFiles("*.sql").Count()} files");
+
             var evolve = new Evolve(cnx, l => logger.LogInformation($"{l}"))
             {
-                Locations = new[] { "db/postgres/migrations" },
+                Locations = new[] { migrationsDirectory.FullName },
                 IsEraseDisabled = true,
             };
+
             evolve.Migrate();
         }
     }
