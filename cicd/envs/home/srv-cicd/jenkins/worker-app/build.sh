b4b3676280b46e63a3ffe086cd9ccfc05723af1b
Yan Tanygin <2025234+FCBshnik@users.noreply.github.com>
2024-01-16 16:11:36 +0300

    fix

--- cicd/envs/home/srv-cicd/jenkins/worker-app/build.sh
+++ cicd/envs/home/srv-cicd/jenkins/worker-app/build.sh
@@ -1,3 +1,3 @@
 cd $WORKSPACE/src/PhysProj/Phys.Lib.App
 version=`date -u +%Y.%m.%d.%H%M`
-dotnet publish --output $WORKSPACE/cicd/envs/home/srv-app/app/bin /p:Version=$version
+dotnet publish --output $WORKSPACE/cicd/envs/home/srv-app/worker-app/bin /p:Version=$version
