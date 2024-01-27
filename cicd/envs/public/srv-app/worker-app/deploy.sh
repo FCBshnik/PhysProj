8f32792ff9c5833d233089eb9703f17866ecb2a6
Yan Tanygin <2025234+FCBshnik@users.noreply.github.com>
2024-01-27 23:07:19 +0300

    ci(github): fix docker deploy

--- cicd/envs/public/srv-app/worker-app/deploy.sh
+++ cicd/envs/public/srv-app/worker-app/deploy.sh
@@ -1,1 +1,1 @@
-docker --context physproj-home-app compose --file docker-compose.yml --project-name physproj up --detach --build --force-recreate app
+docker --context physproj compose --file docker-compose.yml --project-name physproj up --detach --build --force-recreate worker-app
