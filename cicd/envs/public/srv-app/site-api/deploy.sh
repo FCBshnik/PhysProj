fe1364640baeee4fbe2cb5eeb2611a2e91991226
Yan Tanygin <2025234+FCBshnik@users.noreply.github.com>
2024-01-29 20:57:13 +0300

    ci(github): deploy site api app

--- cicd/envs/public/srv-app/site-api/deploy.sh
+++ cicd/envs/public/srv-app/site-api/deploy.sh
@@ -1,1 +1,1 @@
-docker --context physproj-home-app compose --file docker-compose.yml --project-name physproj up --detach --build --force-recreate site-api
+docker --context physproj compose --file docker-compose.yml --project-name physproj up --detach --build --force-recreate site-api
