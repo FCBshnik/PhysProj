c1c74c22f4ba8716af49eb9254b83c4be8777690
Yan Tanygin <2025234+FCBshnik@users.noreply.github.com>
2024-02-06 22:13:25 +0300

    refactor: rename

--- src/PhysProj/Phys.Lib.Site.Api/ApiModule.cs
+++ src/PhysProj/Phys.Lib.Site.Api/ApiModule.cs
@@ -7,18 +7,18 @@ namespace Phys.Lib.Site.Api
     public class ApiModule : Module
     {
         private LoggerFactory loggerFactory;
-        private IConfiguration config;
+        private IConfiguration configuration;
 
         public ApiModule(LoggerFactory loggerFactory, IConfiguration config)
         {
             this.loggerFactory = loggerFactory;
-            this.config = config;
+            this.configuration = config;
         }
 
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterModule(new LoggerModule(loggerFactory));
-            builder.RegisterModule(new DataModule(config, loggerFactory));
+            builder.RegisterModule(new DataModule(configuration, loggerFactory));
             builder.RegisterModule(new CoreModule());
 
             builder.RegisterType<StatusCodeLoggingMiddlware>().AsSelf().SingleInstance();
