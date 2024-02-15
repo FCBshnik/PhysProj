8a632d5d7e50a9845aa7d14cfdfed21638895fd5
Yan Tanygin <2025234+FCBshnik@users.noreply.github.com>
2024-02-15 22:42:27 +0300

    feat(site): add sub works info for collected work

---
+++ src/PhysProj/Phys.Lib.Site.Web/src/lib/components/IconInformation.svelte
@@ -0,0 +1,14 @@
+<button type="button" class="">
+  <svg class="h-5 w-5 text-gray-500" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <title />
+		<g>
+			<g>
+				<g>
+					<circle cx="12" cy="12" fill="none" r="10" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
+					<line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="12" x2="12" y1="12" y2="16" />
+					<line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="12" x2="12" y1="8" y2="8" />
+				</g>
+			</g>
+		</g>
+	</svg>
+</button>
--- src/PhysProj/Phys.Lib.Site.Web/src/lib/components/WorkCard.svelte
+++ src/PhysProj/Phys.Lib.Site.Web/src/lib/components/WorkCard.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import type * as api from "$lib/services/ApiService";
+  import IconInformation from "$lib/components/IconInformation.svelte";
   import DownloadFileButton from "$lib/components/DownloadFileButton.svelte";
+
+  let showInfo = false;
 
   export let work:api.SearchResultWorkModel;
   export let result:api.SearchResultModel;
@@ -15,7 +18,7 @@
   }
 </script>
 
-<div class="border-2 rounded-md m-2 p-2 flex flex-col dark:border-gray-500 border-gray-400">
+<div class="relative border-2 rounded-md m-2 p-2 flex flex-col dark:border-gray-500 border-gray-400">
   <div class="grow flex justify-center items-center">
     <div class="text-center text-ellipsis overflow-hidden text-lg">
       {work.name}
@@ -26,11 +29,38 @@
       <div class="inline-block p-1">{getAuthorShort(author)}</div>
     {/each}
   </div>
-  <div class="text-end text-ellipsis overflow-hidden text-xs">
-    {#each work.files || [] as file}
-      <div class="p-1">
-        <DownloadFileButton file={file}/>
-      </div>
-    {/each}
+  <div class="flex flex-row justify-between">
+    <div class="text-end text-ellipsis overflow-hidden text-xs">
+      {#if work.subWorks?.length}
+      <button type="button" class="" on:click={() => showInfo = !showInfo}>
+        <IconInformation></IconInformation>
+      </button>
+      {/if}
+    </div>
+    <div class="text-end text-ellipsis overflow-hidden text-xs">
+      {#each work.files || [] as file}
+        <div class="p-1">
+          <DownloadFileButton file={file}/>
+        </div>
+      {/each}
+    </div>
   </div>
+
+  {#if showInfo}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+    <div role="tooltip" class="absolute top-0 left-0 w-full min-h-full z-50 border-2 rounded-md text-sm dark:border-gray-600 border-gray-400 dark:bg-gray-800" on:click={() => showInfo = false}>
+      <div class="p-2 rounded-t-md text-center dark:bg-gray-700" >
+          {work.name}
+      </div>
+      <div class="p-2 rounded-md">
+        {#each work.subWorks || [] as subWork}
+          <div>
+            <span class="italic">{subWork.authors?.map(getAuthorShort).join(', ')}</span> - <span>{subWork.name}</span>
+            <span class="italic">{subWork.isTranslation ? ' (Перевод)' : ''}</span>
+          </div>
+        {/each}
+      </div>
+    </div>
+  {/if}
 </div>
